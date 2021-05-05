b7337d64b6a417488c7be18c476b402995754147
emmacivello <ecivello@smith.edu>
2021-05-04 21:22:12 -0400

    Update WOMstory.java

--- WOMstory.java
+++ WOMstory.java
@@ -1531,6 +1531,7 @@ public class WOMstory {	// class header
                         +"\t3) "+foods[2]+"\n");
         String food = "";
         do{
+			op = carl.nextInt();
             if(!(op==1 || op==2 || op==3))
                 System.out.println("Please choose a valid option.");
             else
@@ -1543,6 +1544,7 @@ public class WOMstory {	// class header
                         +"\t3) "+drinks[2]+"\n");
         String drink = "";
         do{
+			op = carl.nextInt();
             if(!(op==1 || op==2 || op==3))
                 System.out.println("Please choose a valid option.");
             else
@@ -1563,7 +1565,7 @@ public class WOMstory {	// class header
         pauseText(2);
         System.out.println("Congratulations!");
         pauseText(2);
-        System.out.println("-=~[YOU WIN]~=-");
+        System.out.println("\n-=~[YOU WIN]~=-");
     }
 	
 	private boolean checkMember(String nameIn) {	// check if you have a particular party member
